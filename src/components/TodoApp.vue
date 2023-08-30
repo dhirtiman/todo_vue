33ca190a1f6f9a58797dcc07d087a2aa0b48585e
dhirtiman <dhirtimankhati@gmail.com>
2023-08-31 00:49:17 +0530

    vueee

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="Contain">
+        <input v-model="todoText" @keyup.enter="addTask" placeholder="enter the task">
+        <button @click="addTask">Add Task</button>
+        <ul>
+            <li v-for="todo in todolist" :key="todo.id" :class="{done: todo.done}">{{ todo.text }}
+                <button @click="todo.done = !todo.done">✔</button>
+                <button @click="removeTask(todo)">❌</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+let id = 0;
+
+const todolist = ref([{
+    id: id++,
+    text: 'cum',
+    done: false,
+}]);
+const todoText = ref('')
+
+
+const addTask = () => {
+    todolist.value.push({
+        id: id++,
+        text: todoText.value,
+        done: false,
+    });
+    todoText.value = '';
+
+}
+
+
+
+function removeTask(todo) {
+    todolist.value = todolist.value.filter((t) => t !== todo);
+}
+
+
+
+
+</script>
+
+
+<style>
+.done {
+    text-decoration: line-through;
+}
+</style>
